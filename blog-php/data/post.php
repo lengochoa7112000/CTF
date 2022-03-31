0cbc110d4730371cb4e9c4c81bb752adb64064c5
Lê Ngọc Hoa <68491164+lengochoa7112000@users.noreply.github.com>
2022-03-31 23:52:55 +0700

    Add files via upload

---
+++ blog-php/data/post.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+  $post = 'posts/' . $_GET['post'];
+  if (isset($_GET['post']) ) {
+    $ok = true;
+  } else {
+    $ok = false;
+    http_response_code(404);
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlentities($_GET['post']) ?></title>
+    <link rel="stylesheet" href="/style.css">
+  </head>
+
+  <body>
+    <?php
+      if ($ok) {
+        echo '<h1>' . htmlentities($_GET['post']) . '</h1><hr><div class="post">';
+        assert("strpos('$post', '..') === false") or die("Attack detected!");
+        include $post;
+        echo '</div>';
+      } else {
+        echo '<h1>post not found :(</h1><hr>';
+      }
+    ?>
+    <p></p>
+    <a href="/">home</a>
+  </body>
+</html>
